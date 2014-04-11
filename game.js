62e3709169c58a287257c92fd9f385428347be32
Krishna Damarla <kdamarla@addus.com>
2014-04-10 19:29:01 -0500

    test

--- game.js
+++ game.js
@@ -2,3 +2,5 @@ var colorsarray = ["#CCCCCC","#333333","#990099","#CD5C5C"];
 for (var i=0; i< 64; i++){
    var rand = Math.floor(Math.random()*colorsarray.length);            document.getElementById("Board").appendChild(document.createElement("div")).style.backgroundColor = colorsarray[rand];    
 }
+
+//test
